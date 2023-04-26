bec9dc2a28437ae5fac1d53d9bcd15240e78cd29
dimka90 <dimkayilrit@gmail.com>
2023-04-26 12:53:24 +0000

    Added some logic to the function

---
+++ _custom_converstion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+* print_String - print value of not printable char
+* @s: string
+* Return: length of the string
+*/
+int print_String(va_list s)
+{
+	int i = 0;
+	int size = 0;
+	unsigned char *str;
+
+	str = va_arg(s, unsigned char *);
+	if (str == NULL)
+	{
+		_putstr("\\x00");
+		return (4);
+	}
+	while (str[i])
+	{
+		if ((str[i] < 32 || str[i] >= 127) && str[i])
+		{
+			_putstr("\\x");
+			hex_S(str[i], &size);
+			size += 2;
+			i++;
+		}
+		else
+		{
+			_putchar(str[i]);
+			i++;
+			size++;
+		}
+	}
+	return (size);
+}

e1c1192bc32ac8ad6ed599176cf1bb0ae31aef56
dimka90 <dimkayilrit@gmail.com>
2023-04-27 04:33:06 +0000

    Added a function that andle the following flag characters for non-custom conversion specifiers:(+,space and #)

--- main.h
+++ main.h
@@ -4,6 +4,19 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
+
+/* FLAGS */
+
+#define F_MINUS 1
+
+#define F_PLUS 2
+
+#define F_ZERO 4
+
+#define F_HASH 8
+
+#define F_SPACE 16
+
 /* lib function*/
 int _printf(const char *_format, ...);
 int _isprintable(int c);
@@ -19,4 +32,5 @@ void print_number_u(unsigned int n, unsigned int *size);
 void _putstr(char *str);
 void hex_S(unsigned int nb, int *size);
 void hex_addr(unsigned long int addr, int *size);
+int get_flags(const char *format, int *i);
 #endif /*MAIN_H*/
